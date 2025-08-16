75d6edd5980e7b142e5567cfc26ffdb212c2bf25
ctrumbla <coltontrumbla@hotmail.com>
2025-08-16 14:41:57 -0500

    Delete hello1 directory

--- hello1/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Dashboard from "./components/Dashboard";
-
-export default function App() {
-  const currentUser = "Alice";
-  return <Dashboard currentUser={currentUser} />;
-}
